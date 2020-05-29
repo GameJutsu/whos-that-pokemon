3116a177a1b1501d3561e349f71fe7b365dceee4
GameJutsu <GameJutsu@users.noreply.github.com>
2020-05-30 04:47:17 +0530

    Initial commit

---
+++ assets/script/script.js
@@ -0,0 +1,34 @@
+const button = document.getElementById("submit");
+const textInput = document.getElementById("textInput");
+const main = document.getElementById("pokemon-image-container");
+const img = document.getElementById("pokemon-image");
+let localData = {};
+
+//Call an id'd Pokemon
+async function calculate(id) {
+  const api = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+  const randNum = Math.floor(Math.random() * 151) + 1;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`);
+  localData = await res.json();
+  img.src = `${api}${idPadding(randNum)}.png`;
+  main.appendChild(img);
+}
+
+//Padding for id
+function idPadding(id) {
+  id = id.toString();
+  if (id < 10) {
+    id = "00" + id;
+  } else if (id < 100) {
+    id = "0" + id;
+  }
+  return id;
+}
+
+calculate();
+
+button.addEventListener("click", () => {
+  if (textInput.value.toLowerCase() == localData.name) {
+    main.classList.add("show");
+  }
+});
