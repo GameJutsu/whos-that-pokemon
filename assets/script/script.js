8c4619022c0f73e558e3e7bf813d7a52fc80c89d
GameJutsu <GameJutsu@users.noreply.github.com>
2020-05-30 05:29:42 +0530

    Added minor functionalities

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,6 @@
 const button = document.getElementById("submit");
+const newPokemon = document.getElementById("newPokemon");
+const reveal = document.getElementById("reveal");
 const textInput = document.getElementById("textInput");
 const main = document.getElementById("pokemon-image-container");
 const img = document.getElementById("pokemon-image");
@@ -6,6 +8,7 @@ let localData = {};
 
 //Call an id'd Pokemon
 async function calculate(id) {
+  textInput.value = "";
   const api = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
   const randNum = Math.floor(Math.random() * 151) + 1;
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`);
@@ -31,4 +34,15 @@ button.addEventListener("click", () => {
   if (textInput.value.toLowerCase() == localData.name) {
     main.classList.add("show");
   }
+});
+
+newPokemon.addEventListener("click", () => {
+  main.classList.remove("show");
+
+  setTimeout(calculate, 700);
+});
+
+reveal.addEventListener("click", () => {
+  main.classList.add("show");
+  textInput.value = localData.name;
 });
